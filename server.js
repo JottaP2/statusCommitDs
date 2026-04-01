3bf79fc120c4ddeb1b2b6e24e00b7041e809739e
JottaP2 <joao.marinho@arapiraca.ufal.br>
2026-04-01 11:49:49 -0300

    fix: formatação do código base

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const fastify = require('fastify')({ logger: true, ignoreTrailingSlash: true });
-
-// O Easypanel vai injetar esse valor através da aba 'Environment'
 const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK;
 
 if (!DISCORD_WEBHOOK_URL) {
@@ -10,8 +8,6 @@ if (!DISCORD_WEBHOOK_URL) {
 fastify.post('/github-event', async (request, reply) => {
     const payload = request.body;
 
-    // 1. Filtro de Segurança e Branch
-    // Verifica se é um push e se é na main (o GitHub envia refs/heads/main) teste
     if (payload.ref === 'refs/heads/main' && payload.head_commit) {
 
         const repoName = payload.repository.name;
@@ -25,14 +21,14 @@ fastify.post('/github-event', async (request, reply) => {
         const horaFormatada = dataObj.toLocaleTimeString('pt-BR', {
             hour: '2-digit',
             minute: '2-digit',
-            timeZone: 'America/Maceio' // Ajustado para o seu fuso em Alagoas
+            timeZone: 'America/Maceio'
         });
 
-        // 3. Montagem do Embed (Visual do Discord)
+
         const discordPayload = {
             embeds: [{
                 title: `🚀 Novo Push no Repositório: ${repoName}`,
-                color: 0x5865F2, // Cor Blurple do Discord
+                color: 0x5865F2,
                 fields: [
                     { name: 'Branch', value: '`main`', inline: true },
                     { name: 'Quem commitou', value: `**${autor}**`, inline: true },
@@ -44,7 +40,6 @@ fastify.post('/github-event', async (request, reply) => {
             }]
         };
 
-        // 4. Envio para o Discord usando a API nativa do Node (fetch)
         try {
             if (!DISCORD_WEBHOOK_URL) {
                 fastify.log.error('DISCORD_WEBHOOK nao configurada. Nao foi possivel enviar notificacao.');
@@ -73,7 +68,6 @@ fastify.post('/github-event', async (request, reply) => {
     return reply.status(200).send({ received: true });
 });
 
-// O Easypanel gerencia a porta, mas o padrão é 3000
 const port = process.env.PORT || 3000;
 
 fastify.listen({ port: port, host: '0.0.0.0' }, (err) => {
